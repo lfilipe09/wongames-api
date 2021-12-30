e515f647cca50bd267956c9e4ce6b137b250aeb2
root <root@DESKTOP-A0HG8BD.localdomain>
2021-12-30 18:58:32 -0300

    feat: Data scrapper creation

--- api/game/services/game.js
+++ api/game/services/game.js
@@ -1,8 +1,57 @@
 'use strict';
 
+const { default: createStrapi } = require("strapi");
+
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-services)
  * to customize this service
  */
+const axios = require("axios")
+const slugify = require("slugify")
 
-module.exports = {};
+async function getGameInfo(slug) {
+  const jsdom = require("jsdom")
+  const { JSDOM } = jsdom
+  const body = await axios.get(`https://www.gog.com/game/${slug}`)
+  const dom = new JSDOM(body.data)
+
+  const description = dom.window.document.querySelector('.description')
+
+  return {
+    rating: `BR0`,
+    short_description: description.textContent.slice(0, 160),
+    description: description.innerHTML
+  }
+}
+
+async function getByName(name, entityName) {
+  const item = await strapi.services[entityName].find({ name })
+  return item.length ? item[0] : null
+}
+
+async function create(name, entityName) {
+  const item = await getByName(name, entityName)
+  if (!item) {
+    return await strapi.services[entityName].create({
+      name,
+      slug: slugify(name, { lower: true })
+    })
+  } else {
+    return item
+  }
+}
+
+module.exports = {
+  populate: async (params) => {
+    const gogPIUrl = "https://www.gog.com/games/ajax/filtered?mediaType=game&page=1&sort=popularity"
+
+    const {
+      data: { products }
+    } = await axios.get(gogPIUrl)
+
+    //console.log(await getGameInfo(products[0].slug))
+
+    await create(products[2].publisher, "publisher")
+    await create(products[2].developer, "developer")
+  }
+};
